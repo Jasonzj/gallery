53bed97b41ec51d23049fa455f1e607ef638e472
Jasonzj <925645402@qq.com>
2017-08-08 16:56:42 +0800

    add example

---
+++ example/src/components/Gallery/Gallery.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Gallery extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div>123</div>
+        )
+    }
+}
+
+export default Gallery
