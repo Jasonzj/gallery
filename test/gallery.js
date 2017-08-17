bbd442150cf0844b947cdaf4b35f249c272cdbdb
Jasonzj <925645402@qq.com>
2017-08-17 20:54:29 +0800

    update test

--- test/gallery.js
+++ test/gallery.js
@@ -1,6 +1,5 @@
 const setGutter = (x = 10, y = 10) => {
     if (x < 0 || y < 0) {
-        console.error('图片间距必须大于等于0')
         return false
     }
     this.options.gutter = {x, y}
@@ -10,7 +9,6 @@ const setGutter = (x = 10, y = 10) => {
 
 const setPuzzleHeight = (height = 500) =>{
     if (!Number.isInteger(height) || height < 0) {
-        console.error('拼图布局高度必须是正整数')
         return false
     }
     this.options.puzzleHeight = height
@@ -20,7 +18,6 @@ const setPuzzleHeight = (height = 500) =>{
 
 const setWaterfallColumn = (column = 4) => {
     if (!Number.isInteger(column) || column < 0) {
-        console.error('瀑布流列数必须为正整数')
         return false
     }
     this.options.waterfallColumn = column
@@ -30,7 +27,6 @@ const setWaterfallColumn = (column = 4) => {
 
 const setBarrelHeight = (min = 200) => {
     if (!Number.isInteger(min) || min < 0) {
-        console.error('木桶布局最小高度必须为正整数')
         return false
     }
     this.options.barrelMinHeight = min
--- test/test.js
+++ test/test.js
@@ -1,22 +1,31 @@
 const api = require('./gallery.js')
 const expect = require('chai').expect
 
-describe('gallery', () => {
+describe('setGutter', () => {
     it('图片间距必须大于0', () => {
         expect(api.setGutter(-1, 1)).to.be.equal(false)
     })
+})
+
+describe('setPuzzleHeight', () => {
     it('拼图布局高度必须大于0', () => {
         expect(api.setPuzzleHeight(-1)).to.be.equal(false)
     })
     it('拼图布局高度必须是整数', () => {
         expect(api.setPuzzleHeight(0.1)).to.be.equal(false)
     })
+})
+
+describe('setWaterfallColumn', () => {
     it('瀑布流列数必须大于0', () => {
         expect(api.setWaterfallColumn(-1)).to.be.equal(false)
     })
     it('瀑布流列数必须是整数', () => {
         expect(api.setWaterfallColumn(0.1)).to.be.equal(false)
     })
+})
+
+describe('setBarrelHeight', () => {
     it('木桶布局最小高度必须大于0', () => {
         expect(api.setBarrelHeight(-1)).to.be.equal(false)
     })
@@ -24,3 +33,5 @@ describe('gallery', () => {
         expect(api.setBarrelHeight(0.1)).to.be.equal(false)
     })
 })
+
+
