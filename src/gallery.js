07801b644dac7b1e0c3571dc6bb45c5213b0327b
Jasonzj <925645402@qq.com>
2017-09-14 14:07:43 +0800

    gallery config align

--- src/gallery.js
+++ src/gallery.js
@@ -20,22 +20,22 @@
         constructor(containerSelector) {
             this.container = document.querySelector(containerSelector)
             this.options = {
-                layout: 2,                  // 布局类型
-                waterfallColumn: 4,         // 瀑布流布局列数
-                fullScreen: false,           // 是否全屏
-                puzzleHeight: 600,          // 拼图高度
-                barrelMinHeight: 200,       // 木桶布局最小行高     
-                gutter: { x: 10, y: 10 },   // 木桶布局间距
-                images: [],                 // 图片数组
+                layout:          2,                  // 布局类型
+                waterfallColumn: 4,                  // 瀑布流布局列数
+                fullScreen:      false,              // 是否全屏
+                puzzleHeight:    600,                // 拼图高度
+                barrelMinHeight: 200,                // 木桶布局最小行高     
+                gutter:          { x: 10, y: 10 },   // 木桶布局间距
+                images:          [],                 // 图片数组
             }
-            this.galleryBox = null      // 图片容器
-            this.columns = []           // 瀑布流列数组
-            this.nPhotos = []           // 木桶布局未加入行数组
-            this.nPhotosWrap = null     // 木桶布局未加入行容器
-            this.resizeTimer = null     // 木桶布局自适应timer
-            this.onresize = false       // 监听容器宽度
-            this.imgIndex = 0           // 图片索引
-            this.cacheWidth = this.container.clientWidth  // 图片宽度缓存
+            this.galleryBox  = null         // 图片容器
+            this.columns     = []           // 瀑布流列数组
+            this.nPhotos     = []           // 木桶布局未加入行数组
+            this.nPhotosWrap = null         // 木桶布局未加入行容器
+            this.resizeTimer = null         // 木桶布局自适应timer
+            this.onresize    = false        // 监听容器宽度
+            this.imgIndex    = 0            // 图片索引
+            this.cacheWidth  = this.container.clientWidth  // 图片宽度缓存
             
             this.init()
         }
